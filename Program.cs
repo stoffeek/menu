67500a1a5e987fc66ef276bcedea2454750353b4
stoffeek <stoffeek@gmail.com>
2023-10-03 21:51:30 +0200

    first

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SomeMenu
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("First shoit");
+            Console.ReadLine();
+        }
+    }
+}
